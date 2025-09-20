d07b4609e6c316f67330477a0b07dcf32e24857d
DENNIS DRECKS <dreckssuave@gmail.com>
2025-09-20 23:46:28 +0300

    property listing

--- property.js
+++ property.js
@@ -1,49 +1,61 @@
 //All Properties
 export const PROPS = [
   {
-    id: "p1",
-    title: "Coastal Loft with Ocean View",
-    location: "Mombasa",
+    id: "bamburi",
+    title: "1 Bedroom Bamburi",
+    location: "Mombasa-Bamburi-fisheries",
     coords: [-4.0435, 39.6682],
     type: "Apartment",
-    price: 88,
+    price: 3500,
     rating: 4.7,
     guests: 3,
     beds: 2,
     baths: 1,
     amenities: ["Wi‑Fi", "AC", "Kitchen", "Sea view"],
     images: [
-      "https://images.unsplash.com/photo-1505691938895-1758d7feb511?q=80&w=1600&auto=format&fit=crop",
-      "https://images.unsplash.com/photo-1505691723518-36a5ac3b2d89?q=80&w=1600&auto=format&fit=crop",
+      "Houses/1 bedroom/bamburi fisheries/WhatsApp Image 2025-09-01 at 12.03.57.jpeg",
+      "Houses/1 bedroom/bamburi fisheries/WhatsApp Image 2025-09-01 at 12.03.59 (1).jpeg",
+      "Houses/1 bedroom/bamburi fisheries/WhatsApp Image 2025-09-01 at 12.03.59 (2).jpeg",
+      "Houses/1 bedroom/bamburi fisheries/WhatsApp Image 2025-09-01 at 12.03.59.jpeg",
+      "Houses/1 bedroom/bamburi fisheries/WhatsApp Image 2025-09-01 at 12.04.00 (1).jpeg",
+      "Houses/1 bedroom/bamburi fisheries/WhatsApp Image 2025-09-01 at 12.04.00 (2).jpeg",
+      "Houses/1 bedroom/bamburi fisheries/WhatsApp Image 2025-09-01 at 12.04.00 (3).jpeg",
+      "Houses/1 bedroom/bamburi fisheries/WhatsApp Image 2025-09-01 at 12.04.00.jpeg"
     ],
     description:
       "Bright, airy loft right on the coast. Wake up to the sound of waves and sip coffee on your private balcony.",
   },
   {
-    id: "p2",
-    title: "Safari Chic Cottage",
-    location: "Nairobi",
+    id: "nyali",
+    title: "Modern 1 Bedroom Nyali",
+    location: "Nyali-Pembeni road",
     coords: [-1.286389, 36.817223],
-    type: "House",
-    price: 120,
+    type: "Apartment",
+    price: 4000,
     rating: 4.9,
     guests: 5,
-    beds: 3,
+    beds: 2,
     baths: 2,
     amenities: ["Wi‑Fi", "Parking", "Outdoor area", "Smart TV"],
     images: [
-      "https://images.unsplash.com/photo-1459535653751-d571815e906b?q=80&w=1600&auto=format&fit=crop",
-      "https://images.unsplash.com/photo-1505691938895-1758d7feb511?q=80&w=1600&auto=format&fit=crop",
+      "Houses/1 bedroom/nyali 1/4c64a1be-018b-494c-acb6-2a0e3da7f4a9.jpg",
+      "Houses/1 bedroom/nyali 1/26e30021-3360-493a-a015-926c9464aa8c.jpg",
+      "Houses/1 bedroom/nyali 1/56c1c4b1-7206-415b-95da-74869230bce2.jpg",
+      "Houses/1 bedroom/nyali 1/d6b49535-09a2-4e3f-98f0-6270f24a71d2.jpg",
+      "Houses/1 bedroom/nyali 1/d8e1df63-6ed3-4805-99a2-b95d76794fec.jpg",
+      "Houses/1 bedroom/nyali 1/d07626eb-e944-4bfb-9961-923d6fe7ddc0.jpg",
+      "Houses/1 bedroom/nyali 1/e618c212-44c9-4dff-a73f-a7c112d6e9f0.jpg",
+      "Houses/1 bedroom/nyali 1/f21b9ba3-2816-4927-b780-e8cd2917da0c.jpg"
     ],
     description:
       "Modern meets rustic near national parks. Perfect base for weekend adventures.",
   },
   {
-    id: "p3",
-    title: "Urban Studio Near CBD",
+    id: "nyali",
+    title: "1 Bedroom Nyali",
     location: "Nairobi",
     coords: [-1.2833, 36.8167],
-    type: "Studio",
+    type: "Apartment",
     price: 55,
     rating: 4.4,
     guests: 2,
@@ -51,11 +63,404 @@ export const PROPS = [
     baths: 1,
     amenities: ["Wi‑Fi", "Workspace", "Elevator"],
     images: [
-      "https://images.unsplash.com/photo-1460317442991-0ec209397118?q=80&w=1600&auto=format&fit=crop",
-      "https://images.unsplash.com/photo-1505691938895-1758d7feb511?q=80&w=1600&auto=format&fit=crop",
+      "Houses/1 bedroom/nyali b/2d5f684b-6f96-4a84-bd42-d0b6c8a9ce27-2.jpg",
+      "Houses/1 bedroom/nyali b/2d5f684b-6f96-4a84-bd42-d0b6c8a9ce27.jpg",
+      "Houses/1 bedroom/nyali b/3f70acf1-6f36-4d32-ac6f-d5ec3e9c043c.jpg",
+      "Houses/1 bedroom/nyali b/8f63a696-3920-4dde-adb8-1b208b2ae51d.jpg",
+      "Houses/1 bedroom/nyali b/886113ad-4d07-4c48-a0ab-d5d9ad127eb5.jpg",
+      "Houses/1 bedroom/nyali b/6620094c-d345-4ab7-b6e4-fbe427d17550.jpg",
+      "Houses/1 bedroom/nyali b/f1a9c45b-78b0-47b6-b43e-a4507a5a9413.jpg"
+      
     ],
     description:
       "Compact, quiet, and central—ideal for business travelers and minimalist explorers.",
   },
+  {
+    id: "nyali",
+    title: "1 Bedroom Nyali",
+    location: "Nairobi",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/1 bedroom/nyali c/3d252eec-7449-4ddc-b8d1-ba9c2773b958.jpg",
+      "Houses/1 bedroom/nyali c/8b0293f6-63bb-40ca-a447-878e4462bb5f.jpg",
+      "Houses/1 bedroom/nyali c/44c3c54d-8fec-42c0-bc0d-77f4c1725b63.jpg",
+      "Houses/1 bedroom/nyali c/71d324cf-9e99-4c67-a841-890bc0ff8872.jpg",
+      "Houses/1 bedroom/nyali c/627c8920-24b3-4ec9-85d1-72224e6cfb5e.jpg",
+      "Houses/1 bedroom/nyali c/1470f67c-fc76-4cb9-af34-0747d048595a.jpg",
+      "Houses/1 bedroom/nyali c/e4794e27-f680-4b7a-8529-19a5338263a6.jpg",
+      "Houses/1 bedroom/nyali c/ef28130a-a901-4f6d-9e60-506acc706cbb.jpg"
+      
+    ],
+    description:
+      "",
+  },
+  {
+    id: "Nyali",
+    title: "1 Bedroom Nyali",
+    location: "Nairobi",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/1 bedroom/nyali d/7b4dfc8f-cd05-4316-ab31-a85032e82497.jpg",
+      "Houses/1 bedroom/nyali d/91b158ca-ff82-4896-a26e-f5fc35bda315.jpg",
+      "Houses/1 bedroom/nyali d/886113ad-4d07-4c48-a0ab-d5d9ad127eb5.jpg",
+      "Houses/1 bedroom/nyali d/886113ad-4d07-4c48-a0ab-d5d9ad127eb5.jpg",
+      "Houses/1 bedroom/nyali d/c81b064c-0937-4c7b-90a2-c9bc93799f0a.jpg",
+      "Houses/1 bedroom/nyali d/e5d0ec9a-11fe-44ae-ad70-a338ac947dab.jpg",
+      "Houses/1 bedroom/nyali d/e8be651c-f733-4016-bbd5-e419a7701241.jpg"
+      
+      
+    ],
+    description:
+    ""
+      
+  },
+
+  {
+    id: "nyali",
+    title: "1 Bedroom Nyali",
+    location: "Nairobi",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/1 bedroom/nyali e ac/07bdc553-1d1d-4ddc-a82c-eec65fb6e45e.jpg",
+      "Houses/1 bedroom/nyali e ac/8f9925ff-3ddd-4937-8a2a-4dcbbc54377d.jpg",
+      "Houses/1 bedroom/nyali e ac/18c6abb0-07cf-4136-ba9c-a6b88d44c5f7.jpg",
+      "Houses/1 bedroom/nyali e ac/757c43de-b1e1-481b-a5b2-620b7c81c62a.jpg",
+      "Houses/1 bedroom/nyali e ac/32240a43-34d8-4c4c-b57f-852d5ace8bd6.jpg"
+      
+      
+    ],
+    description:
+    ''
+      
+  },
+  {
+    id: "nyali",
+    title: "1 Bedroom Nyali",
+    location: "Nyali plaza",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/1 bedroom/nyali plaza/01c9d280-67b5-4781-8623-369177c74702.jpg",
+      "Houses/1 bedroom/nyali plaza/13d15285-bc65-48f0-970b-24d73f9f664c.jpg",
+      "Houses/1 bedroom/nyali plaza/9762cdd4-4094-433b-9345-d36d4ec90f54.jpg",
+      "Houses/1 bedroom/nyali plaza/4809316a-cb51-4177-a572-96a9acb16c45.jpg",
+      "Houses/1 bedroom/nyali plaza/b1e03066-795a-4b93-b4f3-a44c8f6fadc0.jpg",
+      "Houses/1 bedroom/nyali plaza/bd69f064-0f01-4edc-ad5e-a1e2f7899a8c.jpg",
+      "Houses/1 bedroom/nyali plaza/ec5d6226-f2a9-444a-af07-8e67c7bf2de7.jpg"
+      
+      
+    ],
+    description:
+    ""
+      
+  },
+  {
+    id: "nyali",
+    title: "Standalone 1 bedroom Nyali",
+    location: "Mamba village nyali",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/1 bedroom/nyali standalone/1f6c8d8b-2352-43a2-b164-18e4b96261c9.jpg",
+      "Houses/1 bedroom/nyali standalone/5e82b69c-7501-4c6b-950c-4501735a5e58.jpg",
+      "Houses/1 bedroom/nyali standalone/6c791c1c-0fa8-4333-9108-7fe1cc2f6d5c.jpg",
+      "Houses/1 bedroom/nyali standalone/53d85b85-2a9b-4c9b-8bfe-685a68cfad66.jpg",
+      "Houses/1 bedroom/nyali standalone/70eee520-e72e-4214-8f53-7059a595e4fb.jpg",
+      "Houses/1 bedroom/nyali standalone/72d3a204-e6eb-4428-8d89-e32c4116a745.jpg",
+      "Houses/1 bedroom/nyali standalone/aa4ad22b-d846-49ad-87b7-accad93a9d7d.jpg",
+      "Houses/1 bedroom/nyali standalone/efc7cb32-6c3a-4a84-9482-961398103514.jpg"
+      
+
+      
+    ],
+    description:
+    ""
+      
+  },
+
+  // 2 bedroom
+  {
+    id: "bamburi",
+    title: "2 Bedroom Bamburi",
+    location: "Bamburi",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/2 bedroom/bamburi judy/4cc2abf7-7d4c-4719-90b7-9622a2e332a9.jpg",
+      "Houses/2 bedroom/bamburi judy/9532b4fc-260e-4a41-83d7-52807b0f50ca.jpg",
+      "Houses/2 bedroom/bamburi judy/aa67ed8a-49d0-46a2-8252-d6ed88ee52df.jpg",
+      "Houses/2 bedroom/bamburi judy/b4f272c4-2c76-4e5d-a434-26470d764078.jpg",
+      "Houses/2 bedroom/bamburi judy/b33ac316-659f-490b-a977-73e8e7cbff23.jpg",
+      "Houses/2 bedroom/bamburi judy/bfabc171-008a-41d0-bcdf-5714f8e38a66.jpg",
+      "Houses/2 bedroom/bamburi judy/cf23e791-0e22-44ae-8675-1c1e67ef1244.jpg",
+      "Houses/2 bedroom/bamburi judy/f1ce7792-63aa-476b-99c7-9e180f6f535a-2.jpg",
+      "Houses/2 bedroom/bamburi judy/f1ce7792-63aa-476b-99c7-9e180f6f535a.jpg",
+      "Houses/2 bedroom/bamburi judy/fc07dd5e-64f1-4d01-9c08-2114a995de70.jpg"
+      
+    ],
+    description:
+    ""
+      
+  },
+  // studio
+  
+  {
+    id: "NYALI",
+    title: "Cozy studio Nyali",
+    location: "Nairobi",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/Studio/nyali studio/WhatsApp Image 2025-08-30 at 20.47.15.jpeg",
+      "Houses/Studio/nyali studio/WhatsApp Image 2025-08-30 at 20.47.25 (1).jpeg",
+      "Houses/Studio/nyali studio/WhatsApp Image 2025-08-30 at 20.47.25 (1).jpeg",
+      "Houses/Studio/nyali studio/WhatsApp Image 2025-08-30 at 20.47.25.jpeg",
+      "Houses/Studio/nyali studio/WhatsApp Image 2025-08-30 at 20.47.26 (1).jpeg",
+      "Houses/Studio/nyali studio/WhatsApp Image 2025-08-30 at 20.47.26.jpeg",
+      "Houses/Studio/nyali studio/WhatsApp Image 2025-08-30 at 20.47.27 (1).jpeg",
+      "Houses/Studio/nyali studio/WhatsApp Image 2025-08-30 at 20.47.28 (1).jpeg",
+      "Houses/Studio/nyali studio/WhatsApp Image 2025-08-30 at 20.47.29 (1).jpeg",
+      "Houses/Studio/nyali studio/WhatsApp Image 2025-08-30 at 20.47.30.jpeg",
+      
+      
+      
+    ],
+    description:
+    ''
+      
+  },
+
+  {
+    id: "nyali",
+    title: "1 Bedroom Nyali",
+    location: "Nyali",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/Studio/nyali studio 1/36e761b9-0b46-4f40-9d33-9fc7d7edf5b5.jpg",
+      "Houses/Studio/nyali studio 1/554fab3e-87d8-40f2-a102-32e7140dcd47.jpg",
+      "Houses/Studio/nyali studio 1/87409683-275c-470c-b520-501c7c3357a1.jpg",
+      "Houses/Studio/nyali studio 1/cd851e93-5cec-427a-a016-761a91c3f471.jpg",
+      "Houses/Studio/nyali studio 1/e6676423-720c-4297-9967-413c3f4df83f.jpg"
+      
+    ],
+    description:
+    ""
+      
+  },
+  {
+    id: "kilimani",
+    title: "2 Bedroom Kilimani",
+    location: "Heartland Gardens",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.03.jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.04 (1).jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.04.jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.05 (1).jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.05 (2).jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.05.jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.06 (1).jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.06.jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.07 (1).jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.08.jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.09.jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.10.jpeg",
+      "Houses/2 bedroom/kilimani-heartland/WhatsApp Image 2025-08-25 at 10.44.11.jpeg"
+      
+      
+    ],
+    description:
+    ""
+      
+  },
+  {
+    id: "diani",
+    title: "Beachfront Studio",
+    location: "Nairobi",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/Studio/diani beachfront/WhatsApp Image 2025-04-10 at 09.39.07.jpeg",
+      "Houses/Studio/diani beachfront/WhatsApp Image 2025-04-10 at 09.39.08 (1).jpeg",
+      "Houses/Studio/diani beachfront/WhatsApp Image 2025-04-10 at 09.39.08 (2).jpeg",
+      "Houses/Studio/diani beachfront/WhatsApp Image 2025-04-10 at 09.39.08.jpeg",
+      "Houses/Studio/diani beachfront/WhatsApp Image 2025-04-10 at 09.39.09 (1).jpeg",
+      "Houses/Studio/diani beachfront/WhatsApp Image 2025-04-10 at 09.39.09.jpeg",
+      "Houses/Studio/diani beachfront/WhatsApp Image 2025-04-10 at 09.39.10.jpeg"
+      
+      
+    ],
+    description:
+    ""
+      
+  },
+  {
+    id: "diani",
+    title: "Diani Studio",
+    location: "Nairobi",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/Studio/diani budget studio 1/WhatsApp Image 2025-04-10 at 09.39.54 (1).jpeg",
+      "Houses/Studio/diani budget studio 1/WhatsApp Image 2025-04-10 at 09.39.54 (2).jpeg",
+      "Houses/Studio/diani budget studio 1/WhatsApp Image 2025-04-10 at 09.39.54.jpeg",
+      "Houses/Studio/diani budget studio 1/WhatsApp Image 2025-04-10 at 09.39.55 (1).jpeg",
+      "Houses/Studio/diani budget studio 1/WhatsApp Image 2025-04-10 at 09.39.55.jpeg"
+      
+      
+    ],
+    description:
+      ""
+  },
+  {
+    id: "diani",
+    title: "Diani Budget Studio",
+    location: "Diani",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/Studio/diani budget studio 2/WhatsApp Image 2025-04-10 at 09.40.22 (1).jpeg",
+      "Houses/Studio/diani budget studio 2/WhatsApp Image 2025-04-10 at 09.40.22 (2).jpeg",
+      "Houses/Studio/diani budget studio 2/WhatsApp Image 2025-04-10 at 09.40.22.jpeg",
+      "Houses/Studio/diani budget studio 2/WhatsApp Image 2025-04-10 at 09.40.23.jpeg"
+      
+      
+    ],
+    description:
+    ""
+      
+  },
+  {
+    id: "diani",
+    title: "Executive Studio Diani",
+    location: "Nairobi",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/Studio/executive diani/WhatsApp Image 2025-04-10 at 09.39.32 (1).jpeg",
+      "Houses/Studio/executive diani/WhatsApp Image 2025-04-10 at 09.39.32.jpeg",
+      "Houses/Studio/executive diani/WhatsApp Image 2025-04-10 at 09.39.33 (1).jpeg",
+      "Houses/Studio/executive diani/WhatsApp Image 2025-04-10 at 09.39.33.jpeg",
+      "Houses/Studio/executive diani/WhatsApp Image 2025-04-10 at 09.39.34 (1).jpeg",
+      "Houses/Studio/executive diani/WhatsApp Image 2025-04-10 at 09.39.34 (2).jpeg",
+      "Houses/Studio/executive diani/WhatsApp Image 2025-04-10 at 09.39.34.jpeg",
+      "Houses/Studio/executive diani/WhatsApp Image 2025-04-10 at 09.39.35.jpeg"
+      
+      
+    ],
+    description:
+    ""
+      
+  },
+  {
+    id: "shanzu",
+    title: "Shanzu Studio",
+    location: "shanzu",
+    coords: [-1.2833, 36.8167],
+    type: "Apartment",
+    price: 55,
+    rating: 4.4,
+    guests: 2,
+    beds: 1,
+    baths: 1,
+    amenities: ["Wi‑Fi", "Workspace", "Elevator"],
+    images: [
+      "Houses/Studio/shanzu irene/1da282e5-615f-40ec-938c-e8f5f8885a7b.jpg",
+      "Houses/Studio/shanzu irene/2ddbed48-d308-4f99-b960-cad7aa5988fc.jpg",
+      "Houses/Studio/shanzu irene/4a16e18e-4721-4f68-aa42-9d6dba43e53d.jpg",
+      "Houses/Studio/shanzu irene/d4f345b5-3051-42fd-bf50-2bea08ff0ff9.jpg",
+      "Houses/Studio/shanzu irene/fb237db3-ec42-4510-82aa-40d55b8c5014.jpg",
+      "Houses/Studio/shanzu irene/fb9585ea-71ce-4d7c-a2b3-4b3ee784c17e.jpg"
+      
+      
+    ],
+    description:
+    ""
+      
+  },
+
 ];
 
